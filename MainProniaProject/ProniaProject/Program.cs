a8dcbe87ee7c25048ff8e162dbdf4f2dd26836bf
AzerQasimzade <tu8d910pp@code.edu.az.com>
2023-12-01 09:26:22 +0400

    Layout Service Added

--- MainProniaProject/ProniaProject/Program.cs
+++ MainProniaProject/ProniaProject/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using ProniaProject.DAL;
+using ProniaProject.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(
     opt=>opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
     );
+builder.Services.AddScoped<LayoutService>();
 var app = builder.Build();
 app.UseStaticFiles();
 app.MapControllerRoute(
---
+++ MainProniaProject/ProniaProject/Services/LayoutService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProniaProject.DAL;
+
+namespace ProniaProject.Services
+{
+    public class LayoutService
+    {
+        private readonly AppDbContext _context;
+
+        public LayoutService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Dictionary<string, string>> GetSettingsAsync()
+        {
+            Dictionary<string,string> settings =await _context.Settings.ToDictionaryAsync(s=>s.Key,s=>s.Value);
+            return settings;
+
+        }
+    }
+}
